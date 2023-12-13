a4b43fa8c4ccccf2ac6e0a7ab30552be223f0207
Dominik Heller <kilianmandscharo@gmail.com>
2023-12-13 15:09:24 +0100

    Add function to read data with empty lines

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,14 @@ pub fn readlines(filename: &str) -> Vec<String> {
         .map(|line| line.to_string())
         .collect::<Vec<String>>();
 }
+
+pub fn readlines_with_empty(filename: &str) -> Vec<String> {
+    let file = fs::read_to_string(format!(
+        "/home/dominik/programming/aoc-2023/input/{filename}.in"
+    ))
+    .expect("could not read file");
+    return file
+        .split("\n")
+        .map(|line| line.to_string())
+        .collect::<Vec<String>>();
+}
